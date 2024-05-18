5b5e2e402e591a2f9c2272077f6b37e422bede40
lonegunman82 <mateusz.pietrzyk@gmail.com>
2024-05-18 15:17:34 +0200

    logowanie

--- src/App.js
+++ src/App.js
@@ -2,28 +2,38 @@
 import './App.css';
 import {useState} from "react";
 
+
 function App() {
     const [email, setEmail] = useState('magda@agh.edu.pl');
+    const [jestZalogowany, setJestZalogowany] = useState(false);
+
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
-    let message;
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>;
-    } else if (email.length < 15) {
-        message = <div>Twój adres e-mail jest w sam raz.</div>;
-    } else {
-        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    function logout() {
+    setJestZalogowany(false);
+    setEmail(" ");
     }
+
+    let content;
+    if (jestZalogowany) {
+        content = <div>
+            <h2>Twoj e-mail to {email}</h2>
+            <button onClick={() => setJestZalogowany(false)}>Wyloguj się</button>
+        </div>
+    } else {
+        content = <div>
+            <input type="text" onChange={handleChange}></input>
+            <button onClick={() => setJestZalogowany(true)}>Zaloguj się</button>
+            </div>
+        }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" value={email} onChange={handleChange}/>
-            <h2>{message}</h2>
+            {content}
         </div>
     );
 }
